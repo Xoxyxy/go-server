ba5d18986899fe9767f4dcf88722a0debdf419d6
Daniil <dvinakovich@gmail.com>
2025-02-20 14:46:11 +0300

    task 1

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+var Task string
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello,", Task)
+}
+
+type requestBody struct {
+	Task string `json:"task"`
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	body := requestBody{}
+	json.NewDecoder(r.Body).Decode(&body)
+	Task = body.Task
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+
+	router.HandleFunc("/api/get", GetHandler).Methods("GET")
+	router.HandleFunc("/api/post", PostHandler).Methods("POST")
+
 	http.ListenAndServe(":8080", router)
 }
