66d0d925c4d83e4736c3e9a920aa39cbcd2c6ae8
Daniil <dvinakovich@gmail.com>
2025-02-21 14:04:10 +0300

    add GORM DB connection and task POST and GET operations

--- main.go
+++ main.go
@@ -2,33 +2,58 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var Task string
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []Task
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello,", Task)
+	if err := DB.Find(&tasks).Error; err != nil {
+		http.Error(w, "Error getting tasks: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tasks)
 }
 
 type requestBody struct {
-	Task string `json:"task"`
+	Text   string `json:"text"`
+	IsDone bool   `json:"is_done"`
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	body := requestBody{}
-	json.NewDecoder(r.Body).Decode(&body)
-	Task = body.Task
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "JSON decoding error: "+err.Error(), http.StatusBadRequest)
+	}
+
+	task := Task{
+		Text:   body.Text,
+		IsDone: body.IsDone,
+	}
+
+	if err := DB.Create(&task).Error; err != nil {
+		http.Error(w, "Error creating task:"+err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(task)
 }
 
 func main() {
+	InitDB()
+	DB.AutoMigrate(&Task{})
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/get", GetHandler).Methods("GET")
-	router.HandleFunc("/api/post", PostHandler).Methods("POST")
+	router.HandleFunc("/api/tasks", GetTasksHandler).Methods("GET")
+	router.HandleFunc("/api/tasks", CreateTaskHandler).Methods("POST")
 
 	http.ListenAndServe(":8080", router)
 }
---
+++ orm.go
@@ -0,0 +1,9 @@
+package main
+
+import "gorm.io/gorm"
+
+type Task struct {
+	gorm.Model
+	Text   string `json:"text"`
+	IsDone bool   `json:"is_done"`
+}
